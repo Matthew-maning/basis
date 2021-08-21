9423bbe9f2c187a43d3b26afaea4a7e191d76d62
Matthew Ma <9152083@qq.com>
2021-08-21 13:30:31 +0800

    Modify Signleton.
    Signed-off-by: Matthew Ma <9152083@qq.com>

--- MCA/DesignPatterns/src/singleton/Singleton_04.java
+++ MCA/DesignPatterns/src/singleton/Singleton_04.java
@@ -19,7 +19,7 @@ public enum Singleton_04 {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             new Thread(()->{
-                System.out.println(Singleton_01.getInstance().hashCode());
+                System.out.println(Singleton_04.getInstance().hashCode());
             }).start();
         }
     }
